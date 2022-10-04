bf4b1eaed66e7b8d8e7e86b54a5f8d3c48a41637
MetaQuotes <job@metaquotes.net>
2022-10-04 12:24:21 +0300

    Svelte template

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script context="module" lang="ts">
+    import { getData } from './utils';
+    import type { ItemData } from './types';
+</script>
+
+<script lang="ts">
+    const temperature = getData<ItemData>('../data/temperature.json');
+    const precipitation = getData<ItemData>('../data/precipitation.json');
+</script>
+
+<main>
+    {#await temperature}
+        <p>waiting...</p>
+    {:then res}
+        <div>
+            Temperature data length: {res.length}
+            <details>
+                <summary>Temperature</summary>
+                {JSON.stringify(res)}
+            </details>
+        </div>
+    {:catch error}
+        <p style="color: red">{error.message}</p>
+    {/await}
+
+    <br>
+
+    {#await precipitation}
+        <p>waiting...</p>
+    {:then res}
+        <div>
+            Precipitation data length: {res.length}
+            <details>
+                <summary>Precipitation</summary>
+                {JSON.stringify(res)}
+            </details>
+        </div>
+    {:catch error}
+        <p style="color: red">{error.message}</p>
+    {/await}
+</main>
+
+<style>
+    
+</style>
