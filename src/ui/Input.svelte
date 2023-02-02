5f54d46d1057dbfda3f8d003ea6ed5df356cf087
Nikita Bychkov <n.bychkov@alumni.nsu.ru>
2023-02-03 00:55:42 +0700

    update input component

--- src/ui/Input.svelte
+++ src/ui/Input.svelte
@@ -42,7 +42,15 @@
   let onClickOutside = () => {
     showOptions = false;
   };
+
+  let onKeyDown = (event) => {
+    if (event.key === 'Escape') {
+      showOptions = false;
+    }
+  };
 </script>
+
+<svelte:window on:keyup|preventDefault={onKeyDown} />
 
 <div class="container">
   <input on:change={onChange} value={innerValue} />
@@ -89,7 +97,7 @@
 
     box-sizing: border-box;
     width: 100%;
-    height: 300px;
+    max-height: 300px;
     padding: 4px;
 
     box-shadow: 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 9px 28px 8px rgba(0, 0, 0, 0.05);
