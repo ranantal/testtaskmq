69aa95cfa91b3d29fa507bac26c936b944333ae3
Nikita Bychkov <n.bychkov@alumni.nsu.ru>
2023-02-03 01:39:42 +0700

    fix input styles

--- src/ui/Input.svelte
+++ src/ui/Input.svelte
@@ -100,6 +100,7 @@
     max-height: 300px;
     padding: 4px;
 
+    background-color: white;
     box-shadow: 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 9px 28px 8px rgba(0, 0, 0, 0.05);
     border-radius: 4px;
   }
