8436885aa04f4a25cf05a7a3d61c0fe94e4e4226
Nikita Bychkov <n.bychkov@alumni.nsu.ru>
2023-02-05 18:45:46 +0700

    add tooltips for graph

--- src/ui/Graph.svelte
+++ src/ui/Graph.svelte
@@ -11,12 +11,29 @@
   let canvasContainer: HTMLElement;
   let canvas: HTMLCanvasElement;
   let context: CanvasRenderingContext2D;
+  let tooltips: HTMLCanvasElement;
+  let tooltipsCtx: CanvasRenderingContext2D;
+
+  // max and min values from data
+  let maxY;
+  let minY;
+  // size of canvas
+  let width;
+  let height;
+  // steps between values in px
+  let stepX;
+  let stepY;
+  // maps painted graph relative coords to actual canvas coords
+  const mapX = (x: number): number => (x * (width - 50)) / width + 25;
+  const mapY = (y: number): number => height - (y * (height - 50)) / height - 25;
 
   onMount(() => {
     // configuring canvas
     context = canvas.getContext('2d');
+    tooltipsCtx = tooltips.getContext('2d');
 
-    const { width, height } = canvasContainer.getBoundingClientRect();
+    width = canvasContainer.getBoundingClientRect().width;
+    height = canvasContainer.getBoundingClientRect().height;
     canvas.width = width;
     canvas.height = height;
   });
@@ -31,7 +48,7 @@
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     // max and min values from data
-    const { maxY, minY } = data.reduce(
+    const minmax = data.reduce(
       (acc, val) => {
         if (val.v < acc.minY) {
           acc.minY = val.v;
@@ -44,13 +61,11 @@
       { minY: Infinity, maxY: -Infinity }
     );
 
-    // steps between values in px
-    const stepX = width / data.length;
-    const stepY = height / (maxY - minY);
+    maxY = minmax.maxY;
+    minY = minmax.minY;
 
-    // maps painted graph relative coords to actual canvas coords
-    const mapX = (x: number): number => (x * (width - 50)) / width + 25;
-    const mapY = (y: number): number => height - (y * (height - 50)) / height - 25;
+    stepX = width / data.length;
+    stepY = height / (maxY - minY);
 
     // drawing axis
     context.beginPath();
@@ -108,15 +123,70 @@
 
     context.stroke();
   };
+
+  let onMouseMove = async (event) => {
+    const _data = await data;
+    if (!_data.length) {
+      return;
+    }
+
+    const layerX = event.layerX;
+
+    // find value to show
+    const valueIndex =
+      layerX >= 25 &&
+      _data.findIndex((_, i) => {
+        if (mapX(i * stepX) >= layerX && mapX((i + 1) * stepX) > layerX) {
+          return true;
+        }
+      });
+
+    clearTooltips();
+
+    if (typeof valueIndex !== 'number' || !_data[valueIndex]) {
+      return;
+    }
+
+    const value = _data[valueIndex];
+    // draw tooltp
+    tooltipsCtx.font = 'norrmal 12px Arial, sans-serif';
+    tooltipsCtx.textAlign = 'left';
+    tooltipsCtx.fillText(`${dateFormat(new Date(value.t), 'dd.mm.yyyy')}, ${value.v}`, 6, 16);
+
+    // make tooltip visiible
+    tooltips.style.zIndex = '1';
+    tooltips.style.opacity = '1';
+    // position tooltip
+    tooltips.style.transform = `translate(${mapX(valueIndex * stepX)}px, ${mapY((value.v - minY) * stepY) - 25}px)`;
+  };
+
+  let clearTooltips = () => {
+    tooltips.style.zIndex = '-1';
+    tooltips.style.opacity = '0';
+    tooltipsCtx.clearRect(0, 0, tooltips.width, tooltips.height);
+  };
 </script>
 
 <div bind:this={canvasContainer}>
-  <canvas bind:this={canvas} />
+  <canvas bind:this={canvas} on:mousemove={onMouseMove} on:mouseleave={clearTooltips} />
+  <canvas class="tooltips" bind:this={tooltips} width={100} height={25} />
 </div>
 
 <style>
   div {
+    position: relative;
     height: 100%;
     width: 100%;
+  }
+
+  .tooltips {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+
+    background-color: white;
+    border-radius: 3px;
+    box-shadow: 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 9px 28px 8px rgba(0, 0, 0, 0.05);
   }
 </style>
